263eb1f440c7d0986c5aa035a7d149141a9f2f44
Verta-master <vertyagina@gmail.com>
2019-03-21 10:35:20 +0600

    Promo

--- gulpfile.js
+++ gulpfile.js
@@ -99,7 +99,7 @@ gulp.task("copy", function () {
   .pipe(gulp.dest("build"));
 });
 
-gulp.task("serve", ["style:dev", "symbols"], function () {
+gulp.task("serve", ["style:dev"], function () {
   server.init({
     server: ".",
     notify: false,
@@ -121,7 +121,7 @@ gulp.task("build", function(fn) {
   run(
     "clean",
     "copy",
-    "symbols",
+//    "symbols",
     "images",
     "style:production",
     "js:production",
